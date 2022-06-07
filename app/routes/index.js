d9bc2922aed12258f6067175c36a7af6a06dffb4
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2022-06-07 20:07:34 +0300

    first commit

---
+++ app/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+require('dotenv').config();
+
+router.get(['/', '/index.html', '/index', '/ctf', '/flag', '/hint'], function(req, res, next) {
+  res.render('index', { title: 'CyberSite' });
+});
+
+router.post('/login', function(req, res) {
+  
+    cookie = req.cookies.isAdmin;    
+    
+    if(cookie == 1) {
+      res.render('secretPage', { hint: process.env.HINT, success: true});
+    }else{
+      res.render('secretPage', {success: false});
+    }
+})
+
+module.exports = router;
